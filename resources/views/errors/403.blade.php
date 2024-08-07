ed999bc1e884b73b44feb7a71be85844f1ce187d
John-Misha <olatayo.john@swadhasoftwares.com>
2024-08-07 14:46:32 +0530

    modified:   resources/views/errors/403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -7,7 +7,7 @@
     <div class="p-5 bg-white text-center">
 
         <h1>403 Forbidden</h1>
-        <p>You do not have permission to access this page.</p>
+        <p class="text-danger">You do not have permission to access this page.</p>
         <a href="{{ route('home') }}" class="btn btn-primary">Go Home</a>
     </div>
 
